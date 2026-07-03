33d4f4aad532ce9425cbe11c71fe9cca9a1e31a8
TheBtwMan <ambtwmlkn@gmail.com>
2026-07-03 13:16:46 +0100

    Update Java version to 17 and add requirements.txt

---
+++ backend/src/main/java/com/marsa/tos/TosApplication.java
@@ -0,0 +1,13 @@
+package com.marsa.tos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/marsa/tos/TosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marsa.tos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
